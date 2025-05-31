2cb66bf9a630275ace26caef974dac516ebe5da0
Yiyang Xiao <04242912@cumt.edu.cn>
2025-05-31 12:34:53 +0800

    彻底完成USMART的简化

--- Core/Src/usart.c
+++ Core/Src/usart.c
@@ -21,7 +21,8 @@
 #include "usart.h"
 
 /* USER CODE BEGIN 0 */
-
+#include "./SYSTEM/sys/sys.h"
+#include "./SYSTEM/usart/usart.h"
 /* USER CODE END 0 */
 
 UART_HandleTypeDef huart1;
@@ -56,35 +57,57 @@ void MX_USART1_UART_Init(void)
 
 }
 
-// void HAL_UART_MspInit(UART_HandleTypeDef* uartHandle)
-// {
+void HAL_UART_MspInit(UART_HandleTypeDef* uartHandle)
+{
 
-//   GPIO_InitTypeDef GPIO_InitStruct = {0};
-//   if(uartHandle->Instance==USART1)
-//   {
-//   /* USER CODE BEGIN USART1_MspInit 0 */
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(uartHandle->Instance==USART1)
+  {
+  /* USER CODE BEGIN USART1_MspInit 0 */
 
-//   /* USER CODE END USART1_MspInit 0 */
-//     /* USART1 clock enable */
-//     __HAL_RCC_USART1_CLK_ENABLE();
+  /* USER CODE END USART1_MspInit 0 */
+    /* USART1 clock enable */
+    __HAL_RCC_USART1_CLK_ENABLE();
 
-//     __HAL_RCC_GPIOA_CLK_ENABLE();
-//     /**USART1 GPIO Configuration
-//     PA9     ------> USART1_TX
-//     PA10     ------> USART1_RX
-//     */
-//     GPIO_InitStruct.Pin = GPIO_PIN_9|GPIO_PIN_10;
-//     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-//     GPIO_InitStruct.Pull = GPIO_NOPULL;
-//     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-//     GPIO_InitStruct.Alternate = GPIO_AF7_USART1;
-//     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    /**USART1 GPIO Configuration
+    PA9     ------> USART1_TX
+    PA10     ------> USART1_RX
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_9|GPIO_PIN_10;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF7_USART1;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-//   /* USER CODE BEGIN USART1_MspInit 1 */
+  /* USER CODE BEGIN USART1_MspInit 1 */
+    GPIO_InitTypeDef gpio_init_struct;
+    if (uartHandle->Instance == USART_UX) /* 如果是串口1，进行串口1 MSP初始化 */
+    {
+      USART_UX_CLK_ENABLE();      /* USART1 时钟使能 */
+      USART_TX_GPIO_CLK_ENABLE(); /* 发送引脚时钟使能 */
+      USART_RX_GPIO_CLK_ENABLE(); /* 接收引脚时钟使能 */
 
-//   /* USER CODE END USART1_MspInit 1 */
-//   }
-// }
+      gpio_init_struct.Pin = USART_TX_GPIO_PIN;             /* TX引脚 */
+      gpio_init_struct.Mode = GPIO_MODE_AF_PP;              /* 复用推挽输出 */
+      gpio_init_struct.Pull = GPIO_PULLUP;                  /* 上拉 */
+      gpio_init_struct.Speed = GPIO_SPEED_FREQ_HIGH;        /* 高速 */
+      gpio_init_struct.Alternate = USART_TX_GPIO_AF;        /* 复用为USART1 */
+      HAL_GPIO_Init(USART_TX_GPIO_PORT, &gpio_init_struct); /* 初始化发送引脚 */
+
+      gpio_init_struct.Pin = USART_RX_GPIO_PIN;             /* RX引脚 */
+      gpio_init_struct.Alternate = USART_RX_GPIO_AF;        /* 复用为USART1 */
+      HAL_GPIO_Init(USART_RX_GPIO_PORT, &gpio_init_struct); /* 初始化接收引脚 */
+
+#if USART_EN_RX
+      HAL_NVIC_EnableIRQ(USART_UX_IRQn);         /* 使能USART1中断通道 */
+      HAL_NVIC_SetPriority(USART_UX_IRQn, 3, 3); /* 抢占优先级3，子优先级3 */
+#endif
+    }
+    /* USER CODE END USART1_MspInit 1 */
+  }
+}
 
 void HAL_UART_MspDeInit(UART_HandleTypeDef* uartHandle)
 {
--- Middlewares/USMART/SYSTEM/usart/usart.c
+++ Middlewares/USMART/SYSTEM/usart/usart.c
@@ -125,39 +125,6 @@ void usart_init(uint32_t baudrate)
     HAL_UART_Receive_IT(&g_uart1_handle, (uint8_t *)g_rx_buffer, RXBUFFERSIZE);
 }
 
-/**
- * @brief       UART底层初始化函数
- * @param       huart: UART句柄类型指针
- * @note        此函数会被HAL_UART_Init()调用
- *              完成时钟使能，引脚配置，中断配置
- * @retval      无
- */
-void HAL_UART_MspInit(UART_HandleTypeDef *huart)
-{
-    GPIO_InitTypeDef gpio_init_struct;
-    if(huart->Instance == USART_UX)                             /* 如果是串口1，进行串口1 MSP初始化 */
-    {
-        USART_UX_CLK_ENABLE();                                  /* USART1 时钟使能 */
-        USART_TX_GPIO_CLK_ENABLE();                             /* 发送引脚时钟使能 */
-        USART_RX_GPIO_CLK_ENABLE();                             /* 接收引脚时钟使能 */
-
-        gpio_init_struct.Pin = USART_TX_GPIO_PIN;               /* TX引脚 */
-        gpio_init_struct.Mode = GPIO_MODE_AF_PP;                /* 复用推挽输出 */
-        gpio_init_struct.Pull = GPIO_PULLUP;                    /* 上拉 */
-        gpio_init_struct.Speed = GPIO_SPEED_FREQ_HIGH;          /* 高速 */
-        gpio_init_struct.Alternate = USART_TX_GPIO_AF;          /* 复用为USART1 */
-        HAL_GPIO_Init(USART_TX_GPIO_PORT, &gpio_init_struct);   /* 初始化发送引脚 */
-
-        gpio_init_struct.Pin = USART_RX_GPIO_PIN;               /* RX引脚 */
-        gpio_init_struct.Alternate = USART_RX_GPIO_AF;          /* 复用为USART1 */
-        HAL_GPIO_Init(USART_RX_GPIO_PORT, &gpio_init_struct);   /* 初始化接收引脚 */
-
-#if USART_EN_RX
-        HAL_NVIC_EnableIRQ(USART_UX_IRQn);                      /* 使能USART1中断通道 */
-        HAL_NVIC_SetPriority(USART_UX_IRQn, 3, 3);              /* 抢占优先级3，子优先级3 */
-#endif
-    }
-}
 
 /**
  * @brief       Rx传输回调函数
@@ -227,7 +194,3 @@ void USART_UX_IRQHandler(void)
  
 
  
-
-
-
-
